14d99ccb66786f2ed0b0100a36a97bc70827b8d4
Aditya <aditya.aa.a@oracle.com>
2025-09-03 19:13:28 +0530

    Code update

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -16,11 +16,12 @@ def factorial(n):
 @pytest.mark.parametrize(
     "num,expected",
     [
-        (0, 1), (1, 1), (2, 2), (3, 6),
+        (0, 1), (1, 1), (2, 2),
         (4, 24), (5, 120), (6, 720),
-        (7, 5040), (8, 40320), (9, 362880),
+        (7, 5040), (8, 40320),
         (10, 3628800), (12, 479001600),(-1, ValueError),
-        (3.5, ValueError), ("a", ValueError)
+        (3.5, ValueError), ("a", ValueError),(20, 2432902008176640000),(None, ValueError), ([], ValueError)
+
     ]
 )
 def test_factorial(num, expected):
